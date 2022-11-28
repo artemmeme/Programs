39ad6aee741821f92afbe80122ee238a9daf1659
Артем Шустов <artemmeme2017@yandex.ru>
2022-11-28 13:12:07 +0300

    программа для вывода кубов

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Напишите число вывода кубов");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int num1 = 1;
+while (i < num + 1)
+{
+num1 = i*i*i;
+Console.Write($"{num1}  ");
+i++;
+}
